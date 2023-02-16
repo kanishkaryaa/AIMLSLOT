1a45c181fd10460e3ba6f61427f58b92b46f09fb
kanishkaryaa <77195854+kanishkaryaa@users.noreply.github.com>
2023-02-16 11:46:15 +0530

    Add files via upload

---
+++ R/AIMLSLOT.R
@@ -0,0 +1,17 @@
+#' Create Histogram, box plot and numeric summary
+#' @export
+#' @param x numeric value
+AIML <- function(x){
+  #1 row and 2 columns
+  par(mfrow = c(1,2))
+  # Histogram
+  hist(x, col = rainbow(30))
+  # box plot
+  boxlpot(x, col = "green")
+  par(mfrow = c(1,1))
+  #numeric summary
+  data.frame(min = min(x),
+             median = median(x),
+             max = max(x))
+
+}
